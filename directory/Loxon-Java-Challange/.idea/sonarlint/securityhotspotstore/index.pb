225bd79ae78a724c853d67aa650492b70dded0b8
moptra <117033540+moptra@users.noreply.github.com>
2023-04-14 12:59:11 +0200

    update

---
+++ directory/Loxon-Java-Challange/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+H
+Loxon-Java-Challange.iml,a\4\a4be8254808fe079336d12832b2780be7dcc35bc
--- directory/Loxon-Java-Challange/src/RSAKeypair/Key.java
+++ directory/Loxon-Java-Challange/src/RSAKeypair/Key.java
@@ -1,47 +1,55 @@
 package RSAKeypair;
 
 import org.apache.commons.codec.BinaryDecoder;
+import org.apache.commons.codec.binary.Base64;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.Signature;
-import java.util.Base64;
+import java.math.BigInteger;
+import java.security.*;
+import java.security.spec.RSAPublicKeySpec;
 
 
 public class Key {
-    public class SignatureVerification {
-        public void main (String[] args) throws Exception {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+
+        public static void main (String[] args) throws Exception {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 
             //2048 bites kulcsot generáljatok
-            keyPairGenerator.initialize(2048);
+          //  keyPairGenerator.initialize(2048);
 
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+           // KeyPair keyPair = keyPairGenerator.generateKeyPair();
 
-            PrivateKey privateKey = keyPair.getPrivate();
+          //  PrivateKey privateKey = keyPair.getPrivate();
 
+       //     PublicKey publicKey = keyPair.getPublic();
+            BigInteger modulus = new BigInteger("17248583783287079903113137271246031861209075473762271003520024344117478675283474652586161596113797206972168679306294164640942848955832300218529636529550445464194716774066718565749101228844792600232789383363676497017462772430420353599188799297798008756348723947738234251055168756629486299664040919845464748942034214756657090869376301609964555677402543150379616661533947903283149921424479647601855093455805446655898038754508361730381033130987866530604396453092334084252521426781012326321553975566200961121597792854921004443825416407210291755507356842478052159273195108200705007330001990318308207739804674561130732346233", 16);
+         //   System.out.println(publicKey);
+            BigInteger exp = new BigInteger("10001", 16);
+            RSAPublicKeySpec spec = new RSAPublicKeySpec(modulus, exp);
+            PublicKey pub = keyFactory.generatePublic(spec);
+            byte[] data = pub.getEncoded();
+            String base64encoded = new String(new Base64().encode(data));
+            System.out.println(base64encoded);
             //a 'content' aláírásához SHA256withRSA algoritmust használjatok
             Signature sign = Signature.getInstance("SHA256withRSA");
-
-            sign.initSign(privateKey);
-            byte[] bytes = "hello how are you".getBytes();
-
-            sign.update(bytes);
-
-            byte[] signature = sign.sign();
-
-            sign.initVerify(keyPair.getPublic());
-            sign.update(bytes);
-
-            boolean bool = sign.verify(signature);
-
-            if(bool) {
-                System.out.println("Signature verified");
-            } else {
-                System.out.println("Signature failed");
-            }
+            System.out.println(sign);
+            sign.initSign();
+//            byte[] bytes = "hello how are you".getBytes();  // ez mi?
+//
+//            sign.update(bytes);
+//
+//            byte[] signature = sign.sign();
+//
+//            sign.initVerify(keyPair.getPublic());
+//            sign.update(bytes);
+//
+//            boolean bool = sign.verify(signature);
+//
+//            if(bool) {
+//                System.out.println("Signature verified");
+//            } else {
+//                System.out.println("Signature failed");
+//            }
 
         }
-    }
+
 }
